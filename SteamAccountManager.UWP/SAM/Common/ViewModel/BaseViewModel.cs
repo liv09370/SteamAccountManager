b7bf2dac458a072f1b7205882bac83e88235088c
sar <sahin.aral@outlook.de>
2021-12-14 11:59:20 +0100

    SAMUWP-1 switching pc

---
+++ SteamAccountManager.UWP/SAM/Common/ViewModel/BaseViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SteamAccountManager.UWP.SAM.Common.ViewModel
+{
+    public abstract class BaseViewModel
+    {
+    }
+}
---
+++ SteamAccountManager.UWP/SAM/Steam/Account/ViewModel/AccountSwitcherViewModel.cs
@@ -0,0 +1,24 @@
+﻿using SteamAccountManager.UWP.SAM.Common.ViewModel;
+using SteamAccountManager.UWP.SAM.Steam.Account.Model;
+using System.Collections.ObjectModel;
+
+namespace SteamAccountManager.UWP.SAM.Steam.Account.ViewModel
+{
+    internal class AccountSwitcherViewModel : BaseViewModel
+    {
+        public ObservableCollection<SteamAccount> Accounts { get; set; }
+
+        public AccountSwitcherViewModel()
+        {
+            Accounts = new ObservableCollection<SteamAccount>() 
+            { 
+                new SteamAccount() 
+                {
+                    SteamId = "450237405214",
+                    Name = "Sahin",
+                    ProfilePicture = "Assets//LockScreenLogo.scale-200.png"
+                } 
+            };
+        }
+    }
+}
